ba298c9fc874eae8d9beb84987ead4e9859e3380
Reny Ramos <renyramosgarcia@gmail.com>
2019-05-03 12:11:53 -0400

    Change in home page for fast access to list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #323232;
+                color: #AAA4A1;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
@@ -81,17 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Listados
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('employe.index') }}">Employees</a>
+                    <a href="{{ route('company.index') }}">Companies</a>
                 </div>
             </div>
         </div>
