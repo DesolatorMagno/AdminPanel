fd8eefbd47ce36052d32037964a4637c9a893ffe
Reny Ramos <renyramosgarcia@gmail.com>
2019-05-28 18:00:56 -0400

    Fix in front for 'Delete'

--- resources/views/partials/datable.blade.php
+++ resources/views/partials/datable.blade.php
@@ -2,6 +2,8 @@
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.css">
 @endpush
 @push('scrypt')
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" type="text/javascript"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.2/jquery-confirm.min.js"></script>
 <!-- Datatables -->
 <script src="{{ asset('atlantis/assets/js/plugin/datatables/datatables.min.js') }}"></script>
 <script>
